60a76da78d75b186650d72d1733e24a36aa32955
robertotto <robertotto@pingidentity.com>
2023-06-19 17:18:26 +0100

    removed keys

--- app/src/main/java/com/pingidentity/emeasa/davincisample/DaVinciEnvironment.java
+++ app/src/main/java/com/pingidentity/emeasa/davincisample/DaVinciEnvironment.java
@@ -6,15 +6,12 @@ import static com.pingidentity.emeasa.davinci.PingOneDaVinci.EU;
 public @interface DaVinciEnvironment {
 
     public static String REGION = EU;
-    //public static String COMPANY_ID = "MY_COMPANY_ID";
-    //public static String API_KEY = "MY_API_KEY";
-    //public static String MAIN_POLICY_ID = "MY_POLICY_ID";
+    public static String COMPANY_ID = "MY_COMPANY_ID";
+    public static String API_KEY = "MY_API_KEY";
+    public static String MAIN_POLICY_ID = "MY_POLICY_ID";
 
 
-    public static String COMPANY_ID = "16902052-7d1e-4cea-9ccd-6333bfccb544";
-    public static String API_KEY = "bbbef0051834a64f043f97dcba81f7f73b244fe83e1b65ea9160fb1c7d58d5d66dd329eac2a69724c75758c7ebe96cb940bd438b49d84c1b4dd7eba9f8cfa8bbe857e7d8c79e81efd3bb53eddcba4ef14f12e5b2f06b16f280136ea3a63377043638ca1f3668b911c309f887af40f84ebd5b14fe81a38a2c7b86d9444126e0d0";
 
-    public static String MAIN_POLICY_ID = "321915496563dc1398cc6157b8f3edf9";
 
 
 }
